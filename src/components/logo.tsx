a2578ca202668382020deec35742a2bb92cfb823
shirley.pham <thipham1123@gmail.com>
2026-05-03 22:10:14 +0700

    🌻030526: logo component

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,15 +4,17 @@ type Props = {
   size?: number;
   color?: string;
   strokeWidth?: number;
-  width?: number;
-  height?: number;
 };
 
-const Logo = ({ size = 18, color, strokeWidth, width, height }: Props) => {
+const Logo = ({ size = 18, color = "#FFFFFF", strokeWidth }: Props) => {
   return (
-    <div className="flex gap-1">
-      <div className="rounded-md bg-primary">
+    <div className="h-9 flex items-center gap-2">
+      <div className="w-9 h-full flex justify-center items-center rounded-md bg-primary shadow-sm">
         <Sparkles size={size} color={color} strokeWidth={strokeWidth} />
+      </div>
+      <div className='h-full py-0.5 flex flex-col justify-between'>
+        <div className='text-lg font-bold leading-4.5 text-ink-900'>Daylumaa</div>
+        <div className='text-xs leading-3 font-light text-ink-400'>Productivity Hub</div>
       </div>
     </div>
   );
