db32d3db371ba30352f400fd1a672620c3f7fda3
shirley.pham <thipham1123@gmail.com>
2026-09-19 18:59:44 +0700

    🌻190926: format and display date - time of day for title

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import WelcomeTitle from '@/components/dashboard/welcome-title';
 import Layout from '@/components/layout';
 
 const Dashboard = () => {
+  
   return (
     <Layout>
       <WelcomeTitle></WelcomeTitle>
--- src/components/dashboard/welcome-title.tsx
+++ src/components/dashboard/welcome-title.tsx
@@ -1,16 +1,37 @@
-import React from 'react';
+'use client';
+import { format } from 'date-fns';
+import { useEffect, useState } from 'react';
 
-type Props = {
-  
-};
+type Props = {};
 
-const WelcomeTitle = ({...props}: Props) => {
+const WelcomeTitle = ({ ...props }: Props) => {
+  const [date, setDate] = useState({
+    date: '',
+    timeOfDay: '',
+  });
+  useEffect(() => {
+    const now = new Date();
+    const hour = now.getHours();
+
+    const timeOfDay =
+      hour >= 5 && hour < 12
+        ? 'morning'
+        : hour >= 12 && hour < 17
+          ? 'afternoon'
+          : hour >= 17 && hour < 21
+            ? 'evening'
+            : 'night';
+    setDate({
+      date: format(now, 'EEEE, MMMM dd, yyyy'),
+      timeOfDay,
+    });
+  }, []);
   return (
     <div className="flex flex-col items-start">
-      <span className="text-body font-semibold text-ink-400">
-        Friday, Sep 18, 206
+      <span className="text-body font-semibold text-ink-400">{date.date}</span>
+      <span className="text-header-1 font-bold">
+        Good {date.timeOfDay}, Shirley
       </span>
-      <span className='text-header-1 font-bold'>Good evening, Shirley</span>
     </div>
   );
 };
